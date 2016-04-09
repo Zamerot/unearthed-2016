25ce4112cc99d3d8cca421f6e09858bb5d7b32ea
Zoe Taylor <zoe.taylor@thalesgroup.com.au>
2016-04-09 16:03:58 +0800

    Adding simple check of x and y coordinates and destination to vessel/cargo validator and fixing gitignore

--- src/main/java/com/thales/model/Vessel.java
+++ src/main/java/com/thales/model/Vessel.java
@@ -57,13 +57,40 @@ public final class Vessel {
 		
 	}
 	
-	private static final class DefaultCargoValidator implements CargoValidator {
-
+	private final class DefaultCargoValidator implements CargoValidator {
 		@Override
 		public boolean check(Item item, int x, int y) {
+
+			// Use the dimensions of the vessel to determine the pathway down the centre of the deck.
+			// This is a 2m wide pathway (should be at least 1.5m according to documentation).
+			int invalidX_centre = getDimension().width / 2;
+			if( x == invalidX_centre || x == (invalidX_centre - 1))
+				return false;
+
+			// Use the dimensions of the vessel to determine a pathway across the centre of the deck.
+			// This will be a 1m wide pathway to reach the safehavens on the sides of the deck.
+			int invalidY_centre = getDimension().width / 2;
+			if( y == invalidY_centre )
+				return false;
+
+			// Invalid y values for safehaven on right hand side of deck
+			int invalidY_right = getDimension().width - 1;
+			if( y == invalidY_right)
+				return false;
+
+			// Invalid y values for safehaven on left hand side of deck
+			int invalidY_left = 0;
+			if ( y == invalidY_left )
+				return false;
+
+			// If an item is destined for AU17 then we should not place it in the lower right hand quadrant
+			// of the deck space (starboard aft)
+			if (item.getDestination() == Destination.AU17 && x > (getDimension().width/2) && y > (getDimension().height/2) )
+				return false;
+
 			return true;
 		}
-		
+
 	}
 
 }
