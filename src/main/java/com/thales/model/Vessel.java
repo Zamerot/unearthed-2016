55cef02090d46e84f139fc7063e58a3609a86ffa
Zamerot <JasonSCrowley@hotmail.com>
2016-04-09 20:49:11 +0800

    Merge remote-tracking branch 'remotes/fazio/crisp' into Manifest
    # Conflicts:
    #	src/main/java/com/thales/model/Manifest.java
    #	src/main/java/com/thales/window/deckView/DeckView.java

--- src/main/java/com/thales/model/Vessel.java
+++ src/main/java/com/thales/model/Vessel.java
@@ -3,12 +3,12 @@ package com.thales.model;
 import static com.google.common.base.Preconditions.checkNotNull;
 
 public final class Vessel {
-	
-	private static final CargoValidator DEFAULT_VALIDATOR = new DefaultCargoValidator();
+//
+//	private static final CargoValidator DEFAULT_VALIDATOR = new DefaultCargoValidator();
 
-	public static final Vessel VESSEL16 = new Vessel("Vessel 16", new Dimension(14, 50), DEFAULT_VALIDATOR);
-	public static final Vessel VESSEL7 = new Vessel("Vessel 7", new Dimension(10, 16), DEFAULT_VALIDATOR);
-	public static final Vessel VESSEL2 = new Vessel("Vessel 2", new Dimension(11, 19), DEFAULT_VALIDATOR);
+	public static final Vessel VESSEL16 = new Vessel("Vessel 16", new Dimension(14, 50));
+	public static final Vessel VESSEL7 = new Vessel("Vessel 7", new Dimension(10, 16));
+	public static final Vessel VESSEL2 = new Vessel("Vessel 2", new Dimension(11, 19));
 
 	public static final Vessel[] VESSELS = { VESSEL16, VESSEL7, VESSEL2 };
 
@@ -16,12 +16,11 @@ public final class Vessel {
 
 	private final Dimension dimension;
 
-	private final CargoValidator validator;
+	private  CargoValidator validator;
 	
-	private Vessel(String id, Dimension dimension, CargoValidator validator) {
+	private Vessel(String id, Dimension dimension ) {
 		this.id = checkNotNull(id);
 		this.dimension = checkNotNull(dimension);
-		this.validator = checkNotNull(validator);
 	}
 
 	public String getId() {
